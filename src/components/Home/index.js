48cd4806ee61c1dcb8b74ede2e4df8f33ef19e58
Eugene Kim <ekim1707@gmail.com>
2026-03-28 11:33:33 -0400

    changed iframe src

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,7 +15,7 @@ const Home = () => {
         <iframe
           width="816px"
           height="1056px"
-          src="https://docs.google.com/document/d/e/2PACX-1vTQIheip2y6K5LIWaS7srMAPsO8tE-4uV2t5RGnzRZW57ncF9hmNj9oaoa5KC1UYlOzVA7T9tHA2Jm1/pub?embedded=true"
+          src="https://docs.google.com/document/d/e/2PACX-1vQQD-cGon_X-o1TCnk4q6CNbUbI0MVArSSs_HYws8_quHYCjiZAhKaIOsz6pbcfI1RBefor4UU_mrdu/pub?embedded=true"
         ></iframe>
       </main>
     </div>
